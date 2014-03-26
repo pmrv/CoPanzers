6431eb590d58f8a605cbe3ee084b7f643738105e
Marvin Poul <ponder@creshal.de>
2014-03-26 15:24:17 +0100

    egde cases, edge cases, edge cases

--- examples/demo_turret.py
+++ examples/demo_turret.py
@@ -14,11 +14,10 @@ def main (turret, view):
     
     for b in filter (lambda x: x ["Class"] == "Barrier", view):
         bx, by = b.position
-        dx = bx - x
-        dy = by - y
+        dx, dy = bx - x, by - y
         if dy == 0:
-            cannon.rotation = 0
+            cannon.rotation = 0 + math.pi * (dx < 0)
         else:
-            cannon.rotation = -math.acos (dx / math.sqrt (dx**2 + dy**2))
+            cannon.rotation = math.acos (dx / math.sqrt (dx**2 + dy**2)) * dy / abs (dy)
         
         yield partial (destroy_target, cannon, b)
