13ede41957cf06b969e010ff54bcbdd5015da468
Marvin Poul <ponder@creshal.de>
2014-03-26 16:47:48 +0100

    add attributes for all components + doc strings

--- pytanks/scripts.py
+++ pytanks/scripts.py
@@ -30,6 +30,7 @@ def unsure (meth):
     return f
 
 
+# TODO: We should maybe think about making the interfaces flyweights
 class ROInterface:
     """
     read only interface to the components of a entity
@@ -69,11 +70,33 @@ class ROInterface:
     @property
     @unsure
     def hp (self):
+        """
+        Current hit points of entity.
+        """
         return self.eman.component_for_entity (self.e, Health).hp
 
     @property
     @unsure
+    def max_hp (self):
+        """
+        Maximum hit points of entity.
+        """
+        return self.eman.component_for_entity (self.e, Health).max_hp
+
+    @property
+    @unsure
+    def size (self):
+        """
+        Size of the hit box of the entity.
+        """
+        return self.eman.component_for_entity (self.e, Hitbox).size
+
+    @property
+    @unsure
     def position (self):
+        """
+        Position of the entity.
+        """
         # we make a shallow copy here so the script routine cannot modify the
         # position of themselves or other entities
         return copy (self.eman.component_for_entity (self.e, Position))
@@ -81,25 +104,47 @@ class ROInterface:
     @property
     @unsure
     def rotation (self):
+        """
+        Rotation of the entity, in radians, rotation 0 is parallel to the 
+        x-axis.
+        """
         return self.eman.component_for_entity (self.e, Movement).rotation
 
     @property
     @unsure
     def speed (self):
+        """
+        Speed of the entity as a scalar, in px/s.
+        """
         return self.eman.component_for_entity (self.e, Movement).speed
 
     @property
     @unsure
     def velocity (self):
+        """
+        Velocity of the entity as a 2-tuple, in px/s.
+        """
         m = self.eman.component_for_entity (self.e, Movement)
         return m.dx, m.dy
 
     @property
     @unsure
     def mounts (self):
+        """
+        List of either interfaces of mounted entities or None if the respective
+        mount point is empty.
+        """
         # TODO: not optimal to instanstiate the interface on every call again
         return [ROInterface (m, self.eman) if m else None
             for m in self.eman.component_for_entity (self.e, Mount).mounts]
+
+    @property
+    @unsure
+    def root (self):
+        """
+        Entity this entity is mounted on.
+        """
+        return self.eman.component_for_entity (self.e, Mountable).root
 
 class RWInterface (ROInterface):
     """
@@ -136,23 +181,81 @@ class RWInterface (ROInterface):
     @property
     @unsure
     def mounts (self):
+        """
+        List of either interfaces of mounted entities or None if the respective
+        mount point is empty.
+        """
         # TODO: not optimal to instanstiate the interface on every call again
         return [RWInterface (m, self.eman) if m else None
             for m in self.eman.component_for_entity (self.e, Mount).mounts]
 
     @unsure
     def shoot (self):
+        """
+        Shoots a bullet if this entity is a weapon.
+        """
         self.eman.component_for_entity (self.e, Weapon).triggered = True
 
     @property
     @unsure
     def till_reloaded (self):
+        """
+        Seconds until this weapon can fire again.
+        """
         return self.eman.component_for_entity (self.e, Weapon).till_reloaded
 
     @property
     @unsure
     def reload_time (self):
+        """
+        Seconds between shots.
+        """
         return self.eman.component_for_entity (self.e, Weapon).reload_time
+
+    @property
+    @unsure
+    def bullet_damage (self):
+        """
+        Damage done by the bullets of this weapon.
+        """
+        return self.eman.component_for_entity (
+                self.e,
+                Weapon
+        ).bullet_properties [0]
+
+    @property
+    @unsure
+    def bullet_speed (self):
+        """
+        Speed of the bullets of this weapon, in px/s.
+        """
+        return self.eman.component_for_entity (
+                self.e,
+                Weapon
+        ).bullet_properties [1]
+
+    @property
+    @unsure
+    def bullet_hp (self):
+        """
+        Hit points of the bullets of this weapon.
+        """
+        return self.eman.component_for_entity (
+                self.e,
+                Weapon
+        ).bullet_properties [2]
+
+    @property
+    @unsure
+    def bullet_size (self):
+        """
+        Size of the bullets of this weapon.
+        """
+        return self.eman.component_for_entity (
+                self.e,
+                Weapon
+        ).bullet_properties [3]
+
 
 class EntityView (EntityManager):
     """
